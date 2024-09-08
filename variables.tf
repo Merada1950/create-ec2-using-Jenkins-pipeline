5353bffad070811077a65d4073257b8721d527d3
DevOps and Cloud Engineer Ray Eta. E. <115152999+rayeeta@users.noreply.github.com>
2024-09-08 14:51:09 +0200

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "key_name" {
   description = "The name for the SSH key pair"
   type        = string
-  default     = "terraform-ec2-key"
+  default     = "eks-jenkins-key"
 }
 
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c55b159cbfafe1f0" # Example AMI ID for Amazon Linux 2
+  default     = "ami-0d53d72369335a9d6" # Example AMI ID for Amazon Linux 2
 }
